23c4e419b0efd52b81e72c4d67bcc6eac7e3d07c
ziyalogy <ziyalogy@gmail.com>
2022-08-22 11:57:36 +0300

    first things first

---
+++ html/layouts/jamegafilter/content/grid/filter-list.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * ------------------------------------------------------------------------
+ * JA Filter Plugin - Content
+ * ------------------------------------------------------------------------
+ * Copyright (C) 2004-2016 CFTS - Computer Facilities Technical Services, Ltd. All Rights Reserved.
+ * @license - GNU/GPL, http://www.gnu.org/licenses/gpl.html
+ * Author: Ziyal Amanya
+ * Websites: http://www.cfts.co - http://www.buildal.ug
+ * This file may not be redistributed in whole or significant part.
+ * ------------------------------------------------------------------------
+ */
+
+defined('_JEXEC') or die(); ?>
+<div class="ja-filter-wrapper block filter filter-wrapper">	
+	<div class="block-content filter-content">
+		<div class="filter-current filter-values"></div>
+	  <dl class="filter-options filter-list" id="narrow-by-list">
+	    
+	  </dl>
+	</div> 
+</div>
